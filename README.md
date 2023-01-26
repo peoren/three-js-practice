59532547d1e1dfe7ad6fdeabdebcddc7cf70bc30
peoren <hjw_ready@163.com>
2023-01-26 23:53:51 +0800

    update:加入控制器，坐标轴.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # three-js-practice
 three-js practice preject
+
+## 3d要素
+- scene 场景：一切的根本
+- camera 摄像机：控制以什么角度观察场景
+- renderer 渲染器：用于渲染摄像机拍摄的画面
+- obj 物体：被观察的物体
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import {Camera, PerspectiveCamera, Scene} from "three";
+import {Camera, MeshBasicMaterial, PerspectiveCamera, Scene} from "three";
+//引入gui控制器
+import gui_init from "./dat-gui/index";
 //屏幕宽高
 const SCREEN_WIDTH:number =  window.innerWidth
 const SCREEN_HEIGHT:number = window.innerHeight
@@ -14,7 +16,7 @@ camera.position.set(0,0,10)
 scene.add(camera)
 // 创建立方体
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new  THREE.MeshBasicMaterial({ color: 0xffff00 })
+const cubeMaterial:MeshBasicMaterial = new  THREE.MeshBasicMaterial({ color: 0xffff00 })
 // 创建物体
 const cube = new THREE.Mesh(cubeGeometry,cubeMaterial)
 scene.add(cube)
@@ -25,12 +27,19 @@ renderer.setSize(SCREEN_WIDTH,SCREEN_HEIGHT)
 document.body.appendChild(renderer.domElement)
 // 创建控制器
 const controls:OrbitControls = new OrbitControls(camera,renderer.domElement)
+// 设置阻尼，并需要在每一帧添加update
+controls.enableDamping = true
+// 创建坐标轴
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+// 创建时间
+const clock = new THREE.Clock()
 // 适配屏幕
 window.addEventListener("resize",()=>{
     //更新比例
     const newScreenRadio = window.innerWidth/window.innerHeight
-    camera.aspect = newScreenRadio
     //更新相机投影矩阵
+    camera.aspect = newScreenRadio
     camera.updateProjectionMatrix()
     //更新渲染器
     renderer.setSize(window.innerWidth,window.innerHeight)
@@ -40,8 +49,13 @@ window.addEventListener("resize",()=>{
 })
 // 加载
 const render = ()=>{
+    //做1per/s的速度做匀速运动,5s为周期
+    // const speed = 1 // 1per/s
+    // const allTime = clock.getElapsedTime()
+    // cube.position.x = allTime % 5 *speed
     controls.update()
     renderer.render(scene,camera)
     requestAnimationFrame(render)
 }
 render()
+gui_init(cube)
