eacd2536e075f96a2c5bae660eaa4b9637f1902b
peoren <hjw_ready@163.com>
2023-01-29 22:30:56 +0800

    update:新增纹理.

--- src/main.ts
+++ src/main.ts
@@ -15,20 +15,14 @@ const camera:PerspectiveCamera = new THREE.PerspectiveCamera(50,SCREE_RADIO,.1,1
 camera.position.set(0,0,10)
 scene.add(camera)
 // 创建立方体
-// const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const vertices = new Float32Array([
-    1,1,1,
-    1,-1,1,
-    -1,-1,1,
-    1,1,1,
-    -1,-1,1,
-    -1,1,1
-])
-const geometry = new THREE.BufferGeometry()
-geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
-const cubeMaterial:MeshBasicMaterial = new  THREE.MeshBasicMaterial({ color: 0xffff00 })
+const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+//加载纹理
+const texture:THREE.Texture = new THREE.TextureLoader().load('/public/texture01.jpg')
+
+//设置立方体的颜色
+const cubeMaterial:MeshBasicMaterial = new  THREE.MeshBasicMaterial({ color: 0xffff00,map:texture})
 // 创建物体
-const cube = new THREE.Mesh(geometry,cubeMaterial)
+const cube = new THREE.Mesh(cubeGeometry,cubeMaterial)
 scene.add(cube)
 
 // 创建渲染器
